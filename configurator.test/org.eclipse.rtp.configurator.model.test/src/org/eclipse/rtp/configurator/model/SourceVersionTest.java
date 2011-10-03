2435bc3e22198acb3171fac376751a4e0c552489
bveliev <beyhan@eclipsesource.com>
2011-10-03 14:38:38 +0200

    * local source  model file added

--- configurator.test/org.eclipse.rtp.configurator.model.test/src/org/eclipse/rtp/configurator/model/SourceVersionTest.java
+++ configurator.test/org.eclipse.rtp.configurator.model.test/src/org/eclipse/rtp/configurator/model/SourceVersionTest.java
@@ -1,31 +1,27 @@
-/******************************************************************************* 
-* Copyright (c) 2011 EclipseSource and others. All rights reserved. This
-* program and the accompanying materials are made available under the terms of
-* the Eclipse Public License v1.0 which accompanies this distribution, and is
-* available at http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*   EclipseSource - initial API and implementation
-*******************************************************************************/ 
+/*******************************************************************************
+ * Copyright (c) 2011 EclipseSource and others. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: EclipseSource - initial API and
+ * implementation
+ *******************************************************************************/
 package org.eclipse.rtp.configurator.model;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-
 public class SourceVersionTest {
-  
+
   @Test
   public void testGetValues() {
-    SourceVersion version = new SourceVersion( "1.4", 
-                                               "http://foo.bar", 
-                                               "description", 
+    SourceVersion version = new SourceVersion( "1.4",
+                                               "http://foo.bar",
+                                               "description",
                                                "http://info.bar" );
-    
     assertEquals( "1.4", version.getVersion() );
     assertEquals( "http://foo.bar", version.getRepositoryUrl() );
-    assertNull( version.getFeatures() );
+    assertTrue( version.getFeatures().isEmpty() );
   }
 }
--- core/org.eclipse.rtp.core.service.provider/src/org/eclipse/rtp/configurator/service/provider/internal/util/ConfiguratorModelUtil.java
+++ core/org.eclipse.rtp.core.service.provider/src/org/eclipse/rtp/configurator/service/provider/internal/util/ConfiguratorModelUtil.java
@@ -1,15 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2011 EclipseSource and others. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: EclipseSource - initial API and
+ * implementation
+ *******************************************************************************/
 package org.eclipse.rtp.configurator.service.provider.internal.util;
 
 import java.net.URL;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.rtp.configurator.model.SourceProvider;
 import org.eclipse.rtp.configurator.model.SourceUnMarshaller;
+import org.eclipse.rtp.configurator.service.provider.internal.ProviderActivator;
 
 public class ConfiguratorModelUtil {
 
   private static SourceUnMarshaller sourceUnMarshaller;
   private static SourceProvider sourceProvider;
-  private static String defaultModeURL = "file:///C:/Users/bveliev/org.eclipse.rtp/core/org.eclipse.rtp.core.service.provider/data/real-sources.json";
+  private static String defaultModeURL = "http://foo";
 
   public static void setUnMarshaller( SourceUnMarshaller sourceUnMarshaller ) {
     ConfiguratorModelUtil.sourceUnMarshaller = sourceUnMarshaller;
@@ -36,12 +46,30 @@ public class ConfiguratorModelUtil {
 
   private static SourceProvider getDefaultModel() {
     SourceProvider result = null;
+    String[] modelURLs = new String[]{
+      defaultModeURL, getLocalURL()
+    };
+    for( int i = 0; i < modelURLs.length && result == null; i++ ) {
+      try {
+        URL url = new URL( modelURLs[ i ] );
+        result = sourceUnMarshaller.marshal( url.openStream() );
+      } catch( Exception e ) {
+        System.out.println( "Failed to load model" );
+        e.printStackTrace();
+      }
+    }
+    return result;
+  }
+
+  private static String getLocalURL() {
+    String result = "";
     try {
-      URL url = new URL( defaultModeURL );
-      result = sourceUnMarshaller.marshal( url.openStream() );
+      URL unresolvedURL = FileLocator.find( ProviderActivator.getBundleContext().getBundle(),
+                                            new Path( "data/real-sources.json" ),
+                                            null );
+      result = FileLocator.resolve( unresolvedURL ).toExternalForm();
     } catch( Exception e ) {
-      System.out.println( "Failed to load model" );
-      e.printStackTrace();
+      // Ignore
     }
     return result;
   }
