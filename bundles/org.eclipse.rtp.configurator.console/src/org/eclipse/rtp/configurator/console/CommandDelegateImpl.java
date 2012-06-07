45ded6a8966a390aee7b9d353113d22444942768
Beyhan Veliev <beyhan.veliev@sap.com>
2012-06-07 20:25:33 +0200

    Bug 382043 - Install operation has wrong feedback message
    - license header updated

--- bundles/org.eclipse.rtp.configurator.console/src/org/eclipse/rtp/configurator/console/CommandDelegateImpl.java
+++ bundles/org.eclipse.rtp.configurator.console/src/org/eclipse/rtp/configurator/console/CommandDelegateImpl.java
@@ -4,6 +4,10 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html Contributors: EclipseSource - initial API and
  * implementation
+ * 
+ * Contributors:
+ *     EclipseSource - initial API and implementation
+ *     SAP AG - fix for bug 382043
  *******************************************************************************/
 package org.eclipse.rtp.configurator.console;
 
